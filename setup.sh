2e8482f95a935faceed5c871f516f63d994528a3
Troels Bjørnskov <troels.bjoernskov@teliacompany.com>
2022-03-30 16:08:41 +0200

    Improve setup.sh script
    The script now fails gracefully if it detects an existing zshrc or
    zshenv

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env zsh
-ln -s "${0:A:h}/zshrc" "${ZDOTDIR:-$HOME}/.zshrc"
-ln -s "${0:A:h}/zshenv" "${ZDOTDIR:-$HOME}/.zshenv"
+
+local zshrc="${ZDOTDIR:-$HOME}/.zshrc"
+local zshenv="${ZDOTDIR:-$HOME}/.zshenv"
+
+[[ -f "$zshrc" ]] && print "$zshrc exists already, please move or delete it first" && exit 1
+[[ -f "$zshenv" ]] && print "$zshenv exists already, please move or delete it first" && exit 1
+
+ln -s "${0:A:h}/zshrc" "$zshrc"
+ln -s "${0:A:h}/zshenv" "$zshenv"
 
 source "${ZDOTDIR:-$HOME}/.zshenv"
 source "${ZDOTDIR:-$HOME}/.zshrc"
